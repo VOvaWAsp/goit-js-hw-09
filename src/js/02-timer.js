38e3d668748e17a361c05e9fa766fce0e5ab8e8f
VOvaWAsp <127133873+VOvaWAsp@users.noreply.github.com>
2023-11-29 09:51:06 +0100

    final

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -31,7 +31,28 @@ const options = {
 
 const calendar = flatpickr(datetimePicker, options)
 
-selectors.startBtn.addEventListener("click", handleClick) 
+  selectors.startBtn.addEventListener("click", handleClick);
+
+function convertMs(ms) {
+  selectors.startBtn.disabled = true;
+  datetimePicker.disabled = true;
+  // Number of milliseconds per unit of time
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+// Remaining days
+const days = Math.floor(ms / day);
+// Remaining hours
+const hours = Math.floor((ms % day) / hour);
+// Remaining minutes
+const minutes = Math.floor(((ms % day) % hour) / minute);
+// Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+return { days, hours, minutes, seconds };
+      }
 
 function handleClick() {
     let timeID = setInterval(() => {
@@ -48,25 +69,6 @@ function handleClick() {
     function addLeadingZero(value, targetLength) {
        return String(value).padStart(targetLength, "0")
     }
-
-    function convertMs(ms) {
-    // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-        }
  }
 
 
